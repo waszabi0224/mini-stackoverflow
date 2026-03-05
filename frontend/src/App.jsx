6d11d90547c1c968710f2c64009bdd3f50ff35dc
Szabolcs Tóth <tothszabi0224@gmail.com>
2026-03-06 00:59:52 +0100

    frontend megvalósításának első lépése (Fixed #12)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import TicketDetail from "./pages/TicketDetail.jsx";
+import NewTicket from "./pages/NewTicket.jsx";
+import Profile from "./pages/Profile.jsx";
+import Tickets from "./pages/Tickets.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <nav className="flex gap-4 mb-6">
+          <Link to="/tickets">Ticketek</Link>
+          <Link to="/new-ticket">Új ticket</Link>
+          <Link to="/profile">Profil</Link>
+          <Link to="/login">Bejelentkezés</Link>
+          <Link to="/register">Regisztráció</Link>
+          <Link to="/tickets/:id">Ticket részletek</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Navigate to="/tickets" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/tickets" element={<Tickets />} />
+          <Route path="/tickets/:id" element={<TicketDetail />} />
+          <Route path="/new-ticket" element={<NewTicket />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
 export default App
